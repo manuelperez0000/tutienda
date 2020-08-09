de840b01b095e5def4c4501df89bb58f4d22f362
Manuel Perez <manuelperez.0000@gmail.com>
2020-08-09 11:22:41 -0400

    avanso vue

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -29,23 +29,20 @@ var auth = firebase.auth();
 var db = firebase.firestore();
 var stor = firebase.storage(); 
 
-
-
-
-
-
 router.get('/api/auth', (req, res)=>{ 
   if(auth.currentUser){
-    var datos = { estado:true, email:auth.currentUser.email }
+    var datos = { estado:true, email:auth.currentUser.email,uid:auth.currentUser.uid }
     console.log(datos)
     res.json(datos)
-  }else{ console.log(datos); res.json({ estado:false,email:null })} 
+  }else{ console.log(datos); res.json({ estado:false,email:null,uid:null })} 
 });
+
+
 
 router.get('/api/logout', (req, res)=>{
   auth.signOut().then(()=>{
-    res.send('index');
-  });
+    res.json({estado:true});
+  }).catch(err => res.json({estado:false}))
 });
 
 
@@ -60,93 +57,88 @@ router.post('/api/login', (req, res)=>{
     var errorCode = error.code;
     var errorMessage = error.message;
     console.log(errorMessage);
-    res.json(
-      {
-        estado:"false",
-        mensaje:"No autenticado",
-        errorMessage:errorMessage,
-        errorCode:errorCode
-      });
-    }); 
-  });
-  
-  router.post('/api/forgot', (req, res)=>{
-    
-    var emailAddress = req.body.email;
-    
-    auth.sendPasswordResetEmail(emailAddress).then(function() {
-      res.send("Correo enviado");
-    }).catch(function(error) {
-      res.send("Ocurrio un error al enviar el correo"+error)
+    res.json({
+      estado:"false",
+      mensaje:"No autenticado",
+      errorMessage:errorMessage,
+      errorCode:errorCode
     });
-  });
+  }); 
+});
   
-  //------------------------------registro de usuario -------------------------
-  router.post('/api/register', (req, res)=>{
-    var e = req.body.email;
-    var p = req.body.password;
-    console.log(e+" - "+p);
-    //fetch +++++++++++++++++++++++++++++++++++++++++++
-    auth.createUserWithEmailAndPassword(e, p).then(()=>{
-      var id = auth.currentUser.uid; 
-      registro_db(id);
-    })
-    .catch( error => {
-      console.log("Error al crear usuario: "+error.message);
-    });
-    //++++++++++++++++++++++++++++++++++++++++++++++++
-    function registro_db(id){
-      console.log("Inicio de guardado en la base de datos"+req.body.email);
-      var nombre = req.body.nombre;
-      var telefono = req.body.telefono;
-      var email = req.body.email;
-      
-      db.collection("usuarios").doc(id).set({
-        nombre:nombre,
-        telefono:telefono,
-        correo:email
-      }).then(()=>{
-        console.log("guardado con exito")
-        //guardar_tienda(id)
-        res.send("giardado");
-      }).catch(e => {
-        console.log(e)
-        res.send(e);
-      })
-    } 
-  })
+router.post('/api/forgot', (req, res)=>{
   
-/*   router.get('/api/:id', (req, res)=>{
-    var id = req.params.id
-    let tiendas = db.collection('tiendas');
-    let query = tiendas.where('urlunico', '==', id).get()
-    .then(snapshot => {
-      if (snapshot.empty) {
-        res.send('404');
-      }
-      
-      snapshot.forEach(doc => {
-        res.send('mitienda',{
-          resultado:true,
-          id:doc.id,
-          nombre:doc.data().nombre,
-          banner:doc.data().url
-        });
-      });
-    })
-    .catch(err => {
-      res.send('Error obteniendo los datos:', err.message)
-    })
+  var emailAddress = req.body.email;
     
-  })*/
-/* 
-  router.get('/',(req,res) => {
-    res.sendFile(__dirname,"index.html")
+  auth.sendPasswordResetEmail(emailAddress).then(function() {
+    res.json({mensaje:"Se a enviado un link de recuperacion a su orreo"});
+  }).catch(function(error) {
+    res.json({mensaje:"Disculpe: "+error})
+  });
+});
+
+//------------------------------registro de usuario -------------------------
+router.post('/api/register', (req, res)=>{
+  var e = req.body.email;
+  var p = req.body.password;
+  console.log(e+" - "+p);
+
+  auth.createUserWithEmailAndPassword(e, p).then(()=>{
+    var id = auth.currentUser.uid; 
+    registro_db(id);
   })
-   */
-  router.get('/:id', function (req, res) {
-       
-    res.sendFile(path.join(__dirname+"/./../../public/index.html"))
-  }) 
-  
-  module.exports = router;
+  .catch( error => {
+    console.log("Error al crear usuario: "+error.message);
+    res.json({estado:false,mensaje:"Ocurrio un problema al registrar en auth"})
+  });
+
+  function registro_db(id){
+    console.log("Inicio de guardado en la base de datos: "+req.body.email);
+    var nombre = req.body.nombre;
+    var telefono = req.body.telefono;
+    var email = req.body.email;
+    
+    db.collection("usuarios").doc(id).set({
+      nombre:nombre,
+      telefono:telefono,
+      correo:email
+    }).then(()=>{
+      console.log("guardado con exito")
+      //guardar_tienda(id)
+      res.json({estado:true, mensaje:"Guardado con exito"});
+    }).catch(e => {
+      console.log(e)
+      res.json({estado:false,mensaje:"Ocurrio un error al guardar en la base de datos"});
+    })
+  } 
+})
+
+router.get('/api/mitienda',(req,res)=>{
+  db.collection('tiendas').doc("123456").get()
+  .then((doc)=>{
+    res.json({nombre:doc.data().nombre})
+  })
+})
+
+router.post('/api/guardartienda',(req,res)=>{
+ /*  db.collection('tiendas').doc(auth.currentUser.id).set({
+    nombre:req.body.nombre
+  }).then(() =>{
+    res.json({mensaje:"Guardado con exito"})
+  })
+  .catch(err => res.json({mensaje:err.message})) */
+  res.json({mensaje:"guardando bien"})
+})
+
+router.get('/api/urlunico', (req,res)=>{
+db.collection('tiendas').where("urlunico","==",req.body.url)
+.then(()=> res.json({estado:true,mensaje:"disponible"}))
+.catch(err => res.json({estado:false,mensaje:err}))
+})
+
+router.get('/:id', function (req, res) {
+      
+  res.sendFile(path.join(__dirname+"/./../../public/index.html"))
+}) 
+
+module.exports = router;
